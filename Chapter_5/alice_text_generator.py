31de85fb186e5cf75faa779bf0299fa81dfc1374
Frank Facundo <frank.f96@hotmail.com>
2022-10-13 02:45:15 +0200

    One-to-many - Learning to generate text.

--- Chapter_5/alice_text_generator.py
+++ Chapter_5/alice_text_generator.py
@@ -1,8 +1,13 @@
+"""
+One-to-many - Learning to generate text.
+"""
 import os
 import numpy as np
 import re
 import shutil
 import tensorflow as tf
+
+from typing import Dict
 
 DATA_DIR = "./data"
 CHECKPOINT_DIR = os.path.join(DATA_DIR, "checkpoints")
@@ -18,7 +23,7 @@ def download_and_read(urls):
     texts = []
     for i, url in enumerate(urls):
         p = tf.keras.utils.get_file("ex1-{:d}.txt".format(i), url,
-            cache_dir=".")
+                                    cache_dir=".")
         text = open(p, mode="r", encoding="utf-8").read()
         # remove byte order mark
         text = text.replace("\ufeff", "")
@@ -38,13 +43,19 @@ def split_train_labels(sequence):
 
 class CharGenModel(tf.keras.Model):
 
-    def __init__(self, vocab_size, num_timesteps, 
-            embedding_dim, **kwargs):
+    def __init__(self, vocab_size, num_timesteps,
+                 embedding_dim, **kwargs):
         super(CharGenModel, self).__init__(**kwargs)
         self.embedding_layer = tf.keras.layers.Embedding(
             vocab_size,
             embedding_dim
         )
+        """
+        stateful=True: The hidden state output from the previous 
+            training epoch will be used as input to the current epoch.
+        return_sequences=True: The RNN will output at each of the 
+            time steps rather than an aggregate output at the last time steps.
+        """
         self.rnn_layer = tf.keras.layers.GRU(
             num_timesteps,
             recurrent_initializer="glorot_uniform",
@@ -70,7 +81,7 @@ def loss(labels, predictions):
 
 
 def generate_text(model, prefix_string, char2idx, idx2char,
-        num_chars_to_generate=1000, temperature=1.0):
+                  num_chars_to_generate=1000, temperature=1.0):
     input = [char2idx[s] for s in prefix_string]
     input = tf.expand_dims(input, 0)
     text_generated = []
@@ -87,6 +98,7 @@ def generate_text(model, prefix_string, char2idx, idx2char,
     return prefix_string + "".join(text_generated)
 
 
+########################################################################
 # download and read into local data structure (list of chars)
 texts = download_and_read([
     "http://www.gutenberg.org/cache/epub/28885/pg28885.txt",
@@ -96,14 +108,15 @@ clean_logs()
 
 # create the vocabulary
 vocab = sorted(set(texts))
-print("vocab size: {:d}".format(len(vocab)))
+print("vocab: {}".format(vocab))
+print("vocab size: {:d}\n".format(len(vocab)))
 
 # create mapping from vocab chars to ints
-char2idx = {c:i for i, c in enumerate(vocab)}
-idx2char = {i:c for c, i in char2idx.items()}
+char2idx: Dict[str, int] = {c: i for i, c in enumerate(vocab)}
+idx2char: Dict[int, str] = {i: c for c, i in char2idx.items()}
 
 # numericize the texts
-texts_as_ints = np.array([char2idx[c] for c in texts])
+texts_as_ints: np.ndarray = np.array([char2idx[c] for c in texts])
 data = tf.data.Dataset.from_tensor_slices(texts_as_ints)
 
 # number of characters to show before asking for prediction
@@ -116,19 +129,19 @@ sequences = sequences.map(split_train_labels)
 for input_seq, output_seq in sequences.take(1):
     print("input:[{:s}]".format(
         "".join([idx2char[i] for i in input_seq.numpy()])))
-    print("output:[{:s}]".format(
+    print("output:[{:s}]\n".format(
         "".join([idx2char[i] for i in output_seq.numpy()])))
 
 # set up for training
 # batches: [None, 64, 100]
-batch_size = 64
-steps_per_epoch = len(texts) // seq_length // batch_size
+batch_size: int = 64
+steps_per_epoch: int = (len(texts) // seq_length) // batch_size
 dataset = sequences.shuffle(10000).batch(batch_size, drop_remainder=True)
-print(dataset)
+print("dataset : {}\n".format(dataset))
 
 # define network
-vocab_size = len(vocab)
-embedding_dim = 256
+vocab_size: int = len(vocab)
+embedding_dim: int = 256
 
 model = CharGenModel(vocab_size, seq_length, embedding_dim)
 model.build(input_shape=(batch_size, seq_length))
@@ -138,7 +151,7 @@ model.summary()
 for input_batch, label_batch in dataset.take(1):
     pred_batch = model(input_batch)
 
-print(pred_batch.shape)
+print("pred_batch.shape: {}\n".format(pred_batch.shape))
 assert(pred_batch.shape[0] == batch_size)
 assert(pred_batch.shape[1] == seq_length)
 assert(pred_batch.shape[2] == vocab_size)
@@ -147,7 +160,7 @@ model.compile(optimizer=tf.optimizers.Adam(), loss=loss)
 
 # we will train our model for 50 epochs, and after every 10 epochs
 # we want to see how well it will generate text
-num_epochs = 50
+num_epochs: int = 50
 for i in range(num_epochs // 10):
     model.fit(
         dataset.repeat(),
@@ -164,6 +177,6 @@ for i in range(num_epochs // 10):
     gen_model.load_weights(checkpoint_file)
     gen_model.build(input_shape=(1, seq_length))
 
-    print("after epoch: {:d}".format(i+1)*10)
+    print("after epoch: {:d}".format((i+1)*10))
     print(generate_text(gen_model, "Alice ", char2idx, idx2char))
     print("---")
